07e40af3f171044e0d468c63357754e480ee269b
TITUS KILUNDI <muthiani42@gmail.com>
2024-05-03 01:08:16 +0300

    reactNative

--- src/constants/index.js
+++ src/constants/index.js
@@ -65,10 +65,10 @@ const services = [
         title: "Web Developer",
         icon: web,
     },
-    {
-        title: "React Native Developer",
-        icon: mobile,
-    },
+    // {
+    //     title: "React Native Developer",
+    //     icon: mobile,
+    // },
     {
         title: "Backend Developer",
         icon: backend,
